93d5c136263c0011067da7249ccaaf0b68e97058
kessio <64524758+kessio@users.noreply.github.com>
2022-08-30 16:41:17 +0300

    display movies on homepage mobile design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './styles/style.css';
 import movieList from './modules/movie-list.js';
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faMessage } from "@fortawesome/free-solid-svg-icons/faMessage";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons/faThumbsUp";
+
+library.add(faMessage);
+library.add(faThumbsUp);
+
+dom.watch();
 
 movieList();
 
--- src/modules/movie-list.js
+++ src/modules/movie-list.js
@@ -4,14 +4,10 @@ const movieList = async () => {
     const size = 5;
     const firstFiveData = data.slice(0, size);
     firstFiveData.forEach(element => {
-      console.log(firstFiveData);
-      const summ = element.summary;
-      const replced = summ.replace('<p>',' ');
-      const replaceClosed = replced.replace('</p>',' ')
-      
-      const words = replaceClosed.split(' ').slice(0, 8).join(' ');
-      console.log(words);
-
+      const summary = element.summary;
+      const replaceP = summary.replace('<p>',' ');
+      const replaceClosedP = replaceP.replace('</p>',' ')
+      const words = replaceClosedP.split(' ').slice(0, 8).join(' ');
         document.querySelector('.movie-list-wrapper').innerHTML += `
         <div class="movie-list">
         <img class="movie-list-item-img" src="${element.image.medium}">
@@ -19,10 +15,10 @@ const movieList = async () => {
         <div class="movie-name">${element.name}</div>
         <div class="movie-desc">${words}...</div>
         <div class="like-button">
-        <a href="#"><i class="like-btn fas fa-heart"></i></a><span class="likes-count">50 Likes</span>
+        <a href="#"><i class="movie-icon far fa-thumbs-up"></i></a><span class="likes-count">50 Likes</span>
       </div>
         <div class="comment-movie">
-        <a href="#"><i class="like-btn fas fa-thumbs-up"></i></a><span class="likes-count">Comments</span>
+        <a href="#"><i class="movie-icon fas fa-message"></i></a><span class="likes-count">Comments</span>
       </div>
          </div>
         </div>`;
