cf89daede658dea322c7950c40231ca1366a96c4
kessio <64524758+kessio@users.noreply.github.com>
2022-08-31 12:54:54 +0300

    fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './styles/style.css';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faMessage } from '@fortawesome/free-solid-svg-icons/faMessage';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
 import movieList from './modules/movie-list.js';
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faMessage } from "@fortawesome/free-solid-svg-icons/faMessage";
-import { faThumbsUp } from "@fortawesome/free-regular-svg-icons/faThumbsUp";
 
 library.add(faMessage);
 library.add(faThumbsUp);
@@ -10,5 +10,3 @@ library.add(faThumbsUp);
 dom.watch();
 
 movieList();
-
-
--- src/modules/movie-list.js
+++ src/modules/movie-list.js
@@ -1,18 +1,17 @@
 const movieList = async () => {
-    const response = await fetch('https://api.tvmaze.com/shows?page=1');
-    const data = await response.json();
-    const size = 50;
-    const movies = data.slice(0, size);
-    movies.forEach(element => {
-      
-      const summary = element.summary;
-      const replaceP = summary.replace('<p>',' ');
-      const replaceClosedP = replaceP.replace('</p>',' ')
-      const replaceB = replaceClosedP.replace('<b>',' ')
-      const replaceCloseB = replaceB.replace('</b>',' ')
-      const words = replaceCloseB.split(' ').slice(0, 8).join(' ');
+  const response = await fetch('https://api.tvmaze.com/shows?page=1');
+  const data = await response.json();
+  const size = 50;
+  const movies = data.slice(0, size);
+  movies.forEach((element) => {
+    const { summary } = element;
+    const replaceP = summary.replace('<p>', ' ');
+    const replaceClosedP = replaceP.replace('</p>', ' ');
+    const replaceB = replaceClosedP.replace('<b>', ' ');
+    const replaceCloseB = replaceB.replace('</b>', ' ');
+    const words = replaceCloseB.split(' ').slice(0, 8).join(' ');
 
-        document.querySelector('.movie-list-wrapper').innerHTML += `
+    document.querySelector('.movie-list-wrapper').innerHTML += `
         <div class="movie-list">
         <img class="movie-list-item-img" src="${element.image.medium}">
         <div class="movie-list-buttons">
@@ -31,7 +30,7 @@ const movieList = async () => {
       </div>
          </div>
         </div>`;
-    });
-}
+  });
+};
 
 export default movieList;
